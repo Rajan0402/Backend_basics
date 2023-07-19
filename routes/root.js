d68fb46ddd7b5d4053026bc3284a25eaa1f23e50
Rajan Yadav <rajanyadav2873@gmail.com>
2023-07-19 19:36:35 +0530

    code refactoring in MVC

--- routes/root.js
+++ routes/root.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 
+// ^ = should begin with next char, $ = should end with prev char, | = or
+// the regex means route should begin with and end with "/" or it can be "/index.html"
+// (...)? = means whatever under braces is optional
 router.get("^/$|/index(.html)?", (req, res) => {
   // res.sendFile("./views/index.html", { root: __dirname });
   res.sendFile(path.join(__dirname, "..", "views", "index.html"));
