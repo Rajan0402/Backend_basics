a1aa563a929b430b73176c89181d4ce78cf74939
Rajan Yadav <rajanyadav2873@gmail.com>
2023-07-29 19:00:01 +0530

    Added role authentication to access routes

--- controllers/refreshToken.js
+++ controllers/refreshToken.js
@@ -10,25 +10,32 @@ require("dotenv").config();
 
 const handleRefreshToken = (req, res) => {
   const cookies = req.cookies;
-  console.log(cookies);
+  // console.log(cookies);
   if (!cookies?.jwt) return res.sendStatus(401);
-  console.log(cookies.jwt);
+  // console.log(cookies.jwt);
   const refreshToken = cookies.jwt;
 
-  const userExistWithRT = usersDB.users.find(
+  const userExist = usersDB.users.find(
     (person) => person.refreshToken === refreshToken
   );
   try {
-    if (!userExistWithRT) return res.sendStatus(403);
+    if (!userExist) return res.sendStatus(403);
 
     jwt.verify(
       refreshToken,
       process.env.REFRESH_TOKEN_SECRET,
       (err, decoded) => {
-        if (err || userExistWithRT.username !== decoded.username)
+        if (err || userExist.username !== decoded.username)
           return res.sendStatus(403);
+
+        const roles = Object.values(userExist.roles);
         const accessToken = jwt.sign(
-          { username: decoded.username },
+          {
+            userInfo: {
+              username: userExist.username,
+              roles: roles,
+            },
+          },
           process.env.ACCESS_TOKEN_SECRET,
           { expiresIn: "30s" }
         );
